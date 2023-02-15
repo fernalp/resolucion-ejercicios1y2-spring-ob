47a30c6b2763b5f03d918bd4cce0db7162556cb2
fernalp <almanzafernando@hotmail.com>
2023-02-15 16:00:17 -0500

    Resolución ejercicio 1 y 2 del Curso de Spring de Open Bootcamp

---
+++ src/main/java/com/fernandoalmanza/ejercicio1/Saludo.java
@@ -0,0 +1,12 @@
+package com.fernandoalmanza.ejercicio1;
+
+public class Saludo {
+
+    public Saludo() {
+        System.out.println("Se inicializó la clase Saludo");
+    }
+
+    public void imprimirSaludo(){
+        System.out.println("Hola, me place saludarte");
+    }
+}
