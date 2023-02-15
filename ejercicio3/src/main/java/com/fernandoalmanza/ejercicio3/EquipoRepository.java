f2d1d4a12b442ede278b4ae2665b48e3e76f8b44
fernalp <almanzafernando@hotmail.com>
2023-02-15 14:22:06 -0500

    Redistribución de paquetes y resolución del ejercicio 3 del curso de Spring de Open Bootcampo

---
+++ ejercicio3/src/main/java/com/fernandoalmanza/ejercicio3/EquipoRepository.java
@@ -0,0 +1,6 @@
+package com.fernandoalmanza.ejercicio3;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EquipoRepository extends JpaRepository<Equipo, Long> {
+}
